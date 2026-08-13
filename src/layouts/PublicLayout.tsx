d71db3b1b8280cf56ed5f5b1a8cc9c845d4fe932
Sujal Vende <sujalvende9@gmail.com>
2026-08-14 03:52:09 +0530

    Fix: Center align login and signup forms in PublicLayout

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -4,9 +4,9 @@ import Footer from '../components/Footer'
 
 export default function PublicLayout() {
   return (
-    <div className="min-h-screen bg-cream">
+    <div className="min-h-screen bg-cream flex flex-col">
       <Navigation />
-      <main>
+      <main className="flex-1 flex items-center justify-center px-4 py-8">
         <Outlet />
       </main>
       <Footer />
